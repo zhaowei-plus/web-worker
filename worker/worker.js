381a1f48f5f34b3ad6082df26b8d114a46c2494c
zhaowei-plus <1215058679@qq.com>
2019-08-07 20:09:07 +0800

    'sw'

--- worker/worker.js
+++ worker/worker.js
@@ -1,13 +1,9 @@
-/**
- * worker 通过单独的脚本创建
- * **/
-
-// onmessage 定义worker接受消息的回调
-onmessage = (params) => {
-    console.log(params)
-    // postMesage 会把消息发到index.js的worker中
-    postMessage(params)
+function timedCount () {
+    for (let i = 0; i < 10000000000; i++) {
+        if (i % 100000 === 0) {
+            postMessage(i);
+        }
+    }
 }
 
-// 通过close() 也能够马上结束当前的worker线程
-self.close()
+timedCount();
